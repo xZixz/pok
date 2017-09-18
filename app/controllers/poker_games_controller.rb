e54d12439e5ac0db27c265914e8f8facd3a58971
xZixz <kien.le.van.89@gmail.com>
2017-09-18 15:28:37 +0700

    basically edit poker game

--- app/controllers/poker_games_controller.rb
+++ app/controllers/poker_games_controller.rb
@@ -1,4 +1,6 @@
 class PokerGamesController < ApplicationController
+  before_action :get_poker_game, only: [:edit, :update]
+
   def new
     @poker_game = PokerGame.new
   end
@@ -17,8 +19,29 @@ class PokerGamesController < ApplicationController
     redirect_to poker_games_path
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @poker_game.update_attributes(update_params)
+      redirect_to  poker_games_url
+    else
+      flash[:alert] = "Failed updating"
+      render "edit"
+    end
+  end
+
   private
+  def update_params
+    params[:poker_game].permit(:date, user_ids: [])
+  end
+
   def create_params
     params[:poker_game].permit(:date, user_ids: [])
+  end
+
+  def get_poker_game
+    @poker_game = PokerGame.where(id: params[:id]).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :poker_games, only: [:index, :new, :create]
+  resources :poker_games, only: [:index, :new, :create, :edit, :update] 
   resources :users, only: [:show]
 
   devise_for :users
