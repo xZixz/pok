fbe06eaca287e00fe0cdfdce02d81dc9f810c263
xZixz <kien.le.van.89@gmail.com>
2017-09-19 17:07:36 +0700

    add total income, game num to user show

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :poker_enrolls
   has_many :poker_games, through: :poker_enrolls
+  
+  def total_income
+    PokerEnroll.includes(:user).where(user_id: self.id).pluck(:profit).inject(0) do |sum, x|
+      sum ||= 0
+      x ||= 0
+      sum + x
+    end
+  end
+
+  def games_num
+    PokerEnroll.where(user_id: self.id).count
+  end
 end
